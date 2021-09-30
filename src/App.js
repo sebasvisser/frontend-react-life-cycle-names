64d37fe532768c7171a1bb38c332899db69839e2
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-30 15:11:43 +0200

    opdracht opgelost met hulp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {
+    useState
+} from 'react';
 import NameItem from './components/NameItem';
 import './App.css';
 
 function App() {
+    const [activeName, setActiveName] = useState("");
+
   return (
     <>
-      <h1>De actieve naam is: ...</h1>
+      <h1>De actieve naam is: {activeName}</h1>
       <ul>
-        <NameItem name="Henk" />
-        <NameItem name="Piet" />
-        <NameItem name="Jan" />
+        <NameItem name="Henk" setActiveName={setActiveName} activeName={activeName}/>
+        <NameItem name="Piet" setActiveName={setActiveName} activeName={activeName}/>
+        <NameItem name="Jan" setActiveName={setActiveName} activeName={activeName}/>
       </ul>
     </>
   );
--- src/components/NameItem.js
+++ src/components/NameItem.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function NameItem({ name }) {
+function NameItem({ name, activeName, setActiveName }) {
+
+    useEffect(
+        () => {
+            console.log(`${name} is in de Dom geplopt.`);
+        },
+        [name]
+    )
+
+    useEffect(
+        () =>{
+            if( activeName ){
+                console.log(`${activeName} wilt graag een pilsje bestellen.`);
+            }
+        },
+        [activeName]
+    )
+
   return (
     <li>
       <h3>{name}</h3>
-      <button type="button">
+      <button type="button" onClick={() => setActiveName(`${name}`)}>
         Geef een rondje!
       </button>
     </li>
